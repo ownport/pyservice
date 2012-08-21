f2e839f5bb5b485010a6453e03c3a446e07b83d5
Andrey Usov <ownport@gmail.com>
2012-08-21 21:29:27 +0300

    new test cases container for service.py

---
+++ tests/test_service.py
@@ -0,0 +1,12 @@
+import unittest
+import pyservice
+
+class PyServiceTests(unittest.TestCase):
+    
+    def test_initial(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()        
+    
