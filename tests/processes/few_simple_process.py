9f7634a65dc28a9652da6bbc922a2a2fb06a5dc9
Andrey Usov <ownport@gmail.com>
2012-08-23 19:08:33 +0300

    update test processes

--- tests/processes/few_simple_process.py
+++ tests/processes/few_simple_process.py
@@ -1,10 +1,14 @@
 import pyservice
 
 class SimpleProcess1(pyservice.Process):
+    pidfile = ''
+    
     def run(self):
         print 'SimpleProcess1.run()'
 
 class SimpleProcess2(pyservice.Process):
+    pidfile = ''
+
     def run(self):
         print 'SimpleProcess2.run()'
 
--- tests/processes/simple_process.py
+++ tests/processes/simple_process.py
@@ -1,9 +1,11 @@
+import os
+import time
 import pyservice
 
 class SimpleProcess(pyservice.Process):
     
-    pidfile = ''
-    logfile = ''
+    pidfile = os.path.join(os.getcwd(), 'tests/run/simple_process.pid')
+    logfile = os.path.join(os.getcwd(), 'tests/log/simple_process.log')
     
     def run(self):
-        pass
+        time.sleep(10)
