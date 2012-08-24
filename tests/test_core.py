89cf226939e1d819f0d5fe241cb593998688853c
Andrey Usov <ownport@gmail.com>
2012-08-24 13:00:01 +0300

    update core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 import pyservice
 
@@ -8,7 +9,13 @@ class CoreTests(unittest.TestCase):
         process = pyservice.utils.load_process('tests.processes.simple_process.SimpleProcess')
         service = pyservice.Service(process)
         service.start()
-                    
+
+    def test_service_stop(self):
+        time.sleep(1)
+        process = pyservice.utils.load_process('tests.processes.simple_process.SimpleProcess')
+        service = pyservice.Service(process)
+        service.stop()
+                        
         
 if __name__ == '__main__':
     unittest.main(exit=False)        
